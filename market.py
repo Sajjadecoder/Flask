9c2f67dfc4a7df0ee44f433c05bf75451cb51d23
Sajjadecoder <sajjadahmed102005@gmail.com>
2025-08-21 22:09:08 +0500

    templates and passing values in it

--- market.py
+++ market.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>" 
+@app.route("/home")
+#display the home page in both of the routes
+def home_page():
+      return render_template('home.html')
+@app.route('/market')
+def market_page():
+       return render_template('market.html',item_name = 'Phone') 
 
-@app.route('/about')
-def about_page():
-    return "<h1>About page!</h1>" 
+
 
 # dynamic routes
 @app.route('/about/<username>')
