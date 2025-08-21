f315c4ae467a9bf04bc16d077672126477a8ede0
Sajjadecoder <sajjadahmed102005@gmail.com>
2025-08-21 13:05:25 +0500

    routes n dynamic routes

--- market.py
+++ market.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<h1>Hello, World!</h1>" 
+
+@app.route('/about')
+def about_page():
+    return "<h1>About page!</h1>" 
+
+# dynamic routes
+@app.route('/about/<username>')
+def dynamic_about_page(username):
+    return f'<h1>About page of {username}!</h1>' 
