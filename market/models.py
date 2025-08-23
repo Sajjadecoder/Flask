3fa3a6e70dc8e24856eb0b1fe19e0a736bda81c7
Sajjadecoder <sajjadahmed102005@gmail.com>
2025-08-23 11:17:27 +0500

    User model created

--- market/models.py
+++ market/models.py
@@ -1,9 +1,17 @@
 from market import db
+class User(db.Model):
+      user_id = db.Column(db.Integer(),primary_key = True)
+      username = db.Column(db.String(length=12),nullable = False,unique = True) 
+      email = db.Column(db.String(length=20),nullable = False,unique = True)
+      password_hash = db.Column(db.String(length=60),nullable = False) # flask limits hashed password to have max 60 characters
+      budget = db.Column(db.Integer(),nullable =False,default = 2000)
+      items = db.relationship('Item',backref = 'owned_user',lazy = True)
 class Item(db.Model):
       item_id = db.Column(db.Integer(),primary_key = True)
       name = db.Column(db.String(length=30),nullable=False) # name <= 30 characters & not null
       price = db.Column(db.Integer(),nullable=False)
       barcode = db.Column(db.String(length=12),nullable = False,unique = True)
       description = db.Column(db.String(length=100),nullable=False)
+      owner = db.Column(db.Integer(),db.ForeignKey('user.user_id'))
       def __repr__(self):
             return f'Item name: {self.name}'
