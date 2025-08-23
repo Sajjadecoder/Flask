76b243e52135ca96ccd398161e5de62f8ef85724
Sajjadecoder <sajjadahmed102005@gmail.com>
2025-08-23 18:12:52 +0500

    flask forms

--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,6 @@
 from market import app,render_template
 from market.models import Item
-
+from market.forms import RegisterForm
 @app.route("/")
 @app.route("/home")
 #display the home page in both of the routes
@@ -12,7 +12,14 @@ def market_page():
     items = Item.query.all()
     return render_template('market.html',items = items) 
 
-
+@app.route('/register')
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html',form=form)
+@app.route('/login')
+def login_page():
+    # form = RegisterForm()
+    return f'<h1>Login page</h1>'
 
 # dynamic routes
 @app.route('/about/<username>')
